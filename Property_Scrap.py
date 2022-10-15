ead0c808e6a0d44035d400a2ed76627c788b6fc5
Bilal Basharat <bilalbisharat@gmail.com>
2022-10-15 16:18:02 +0500

    scrapping more data with parallel scrapping concept

--- Property_Scrap.py
+++ Property_Scrap.py
@@ -1,5 +1,4 @@
-from http.client import FOUND
-from tkinter import Button
+import concurrent.futures
 from attr import attrs
 from select import select
 from webbrowser import Chrome
@@ -25,49 +24,55 @@ propertyprice = []
 propertyLocation = []
 propertyArea = []
 propertyPurpose = []
-
-filePath = 'E:/BSCS/3rd_Semester_Data/DSA_Lab/MidTerm_Project/CS261F22PID42/PropertyData(Lahore).csv'
+urls = []
+filePath = 'E:/BSCS/3rd_Semester_Data/DSA_Lab/MidTerm_Project/CS261F22PID42/PropertyData(Lahore)New.csv'
 # file = open(filePath, 'r')
 # if(file.read() == None):
 #     file = open(filePath,'a')
 #     writer = csv.writer(file)
 #     writer.writerow['Agency Name','Property Type','Price','Location','Area','Purpose']
-    
+mainURL = "https://www.zameen.com/Rentals/Lahore-1-"
+for i in range(33,100):
+    completeURL = mainURL+str(i)+'.html'
+    urls.append(completeURL)
+
 with open(filePath,'a', newline='') as outfile:
     writer = csv.writer(outfile)
-    urls = "https://www.zameen.com/Homes/Lahore-1-"
-    for i in range(92,200):
-        driver = webdriver.Chrome(executable_path="C:\Program Files (x86)\chromedriver.exe")
-        driver.get(urls+str(i)+'.html')
-        content = driver.page_source
-        soup = BeautifulSoup(content, 'lxml')
-        linksList = soup.findAll('div', class_="f74e80f3")
-        for link in linksList:
-            links = link.find('a',class_="_7ac32433")
-            url = links['href']
-            url = 'https://www.zameen.com' + url 
+    def transform(url):
+            driver = webdriver.Chrome(executable_path="C:\Program Files (x86)\chromedriver.exe")
             driver.get(url)
-            page_Content = driver.page_source
-            soup = BeautifulSoup(page_Content, 'lxml')
-            try:
-                soup.find('div',class_="_5a588edf").text
-            except AttributeError as error:
-                print(error)
-            else:
-                Agency = soup.find('div',class_="_5a588edf").text
-                AgencyName.append(Agency)
-                column = soup.find('ul', class_="_033281ab")
-                type = column.find('span', {"aria-label" : "Type"}).text
-                propertyType.append(type)
-                Price = column.find('span', {"aria-label" : "Price"}).text
-                propertyprice.append(Price)
-                location = column.find('span', {"aria-label" : "Location"}).text
-                propertyLocation.append(location)
-                area= column.find('span', {"aria-label" : "Area"}).text
-                propertyArea.append(area)
-                purpose = column.find('span', {"aria-label" : "Purpose"}).text
-                propertyPurpose.append(purpose)
-                writer.writerow([Agency,type,Price,location,area,purpose])
+            content = driver.page_source
+            soup = BeautifulSoup(content, 'lxml')
+            linksList = soup.findAll('div', class_="f74e80f3")
+            for link in linksList:
+                links = link.find('a',class_="_7ac32433")
+                url = links['href']
+                url = 'https://www.zameen.com' + url 
+                driver.get(url)
+                page_Content = driver.page_source
+                soup = BeautifulSoup(page_Content, 'lxml')
+                try:
+                    soup.find('div',class_="_5a588edf").text
+                except AttributeError as error:
+                    print(error)
+                else:
+                    Agency = soup.find('div',class_="_5a588edf").text
+                    AgencyName.append(Agency)
+                    column = soup.find('ul', class_="_033281ab")
+                    type = column.find('span', {"aria-label" : "Type"}).text
+                    propertyType.append(type)
+                    Price = column.find('span', {"aria-label" : "Price"}).text
+                    propertyprice.append(Price)
+                    location = column.find('span', {"aria-label" : "Location"}).text
+                    propertyLocation.append(location)
+                    area= column.find('span', {"aria-label" : "Area"}).text
+                    propertyArea.append(area)
+                    purpose = column.find('span', {"aria-label" : "Purpose"}).text
+                    propertyPurpose.append(purpose)
+                    writer.writerow([Agency,type,Price,location,area,purpose])
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+            executor.map(transform, urls)
+
 # print(AgencyName)
 # print(propertyType)
 # print(propertyprice)
