7b95f50de71b1d20cd2a1d7c0b8702f3334d5d40
Bilal Basharat <bilalbisharat@gmail.com>
2022-10-28 19:14:27 +0500

    updating algorithms by resolving issues

--- UI/print.py
+++ UI/print.py
@@ -1,11 +1,17 @@
+from ast import And, Or
 import csv
+from operator import and_
+import re
 
   # reading file to show data
 file = csv.reader(open('AllPakPropertyData.csv', 'r'))
 rows = [row for row in file]
 def printArray(rows):
-  length= len(rows)
-  print(rows)
-  print(length)
+  print(rows[1][2])
+  # for row in rows:
+  if ('Crore' in rows[2][2]):
+    price = re.findall(r'[-+]?(?:\d*\.\d+|\d+)',rows[2][2])
+    print(price)
+    
 
 print(printArray(rows))
